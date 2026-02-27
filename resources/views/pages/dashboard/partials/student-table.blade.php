beb7c87a2f9cbc542e99adc8318ce0f6dcf28ae8
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2026-02-27 13:22:43 +0100

    TB fix : fix front for dashboard

--- resources/views/pages/dashboard/partials/student-table.blade.php
+++ resources/views/pages/dashboard/partials/student-table.blade.php
@@ -1,10 +1,10 @@
-<div class="bg-white rounded-2xl shadow-sm border border-gray-100">
+<div class="bg-white rounded-2xl shadow-sm border border-gray-200 dark:border-gray-700 dark:bg-gray-800">
 
     <!-- Header -->
-    <div class="flex items-center justify-between px-6 py-4 border-b border-gray-100">
+    <div class="flex items-center justify-between px-6 py-4 border-b border-gray-200 dark:border-gray-700">
         <div>
-            <h3 class="text-lg font-semibold text-gray-800">
-                Etudiant
+            <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Etudiants
             </h3>
             <p class="text-sm text-gray-500">
                 Liste des étudiants récement ajoutés
@@ -20,26 +20,26 @@
     <!-- Table -->
     <div class="overflow-x-auto">
         <table class="w-full text-sm text-left">
-            <thead class="bg-gray-50 text-gray-600 uppercase text-xs">
-            <tr>
+            <thead class="bg-gray-50 dark:bg-gray-800 text-gray-600 uppercase text-xs">
+            <tr class="text-left text-xs font-semibold text-gray-500 uppercase tracking-wider dark:text-white">
                 <th class="px-6 py-3">Nom</th>
                 <th class="px-6 py-3">Prénom</th>
                 <th class="px-6 py-3">Email</th>
             </tr>
             </thead>
 
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
             @forelse($students as $student)
-                <tr class="hover:bg-gray-50 transition">
-                    <td class="px-6 py-4 font-medium text-gray-800">
+                <tr class="hover:bg-gray-200 dark:hover:bg-gray-700 transition">
+                    <td class="px-6 py-4 font-medium text-gray-800 dark:text-white">
                         {{ $student->last_name }}
                     </td>
 
-                    <td class="px-6 py-4 text-gray-600">
+                    <td class="px-6 py-4 text-gray-600 dark:text-white">
                         {{$student->first_name}}
                     </td>
 
-                    <td class="px-6 py-4 text-gray-600">
+                    <td class="px-6 py-4 text-gray-600 dark:text-white">
                         {{$student->email}}
                     </td>
                 </tr>
--- resources/views/pages/dashboard/partials/teacher-table.blade.php
+++ resources/views/pages/dashboard/partials/teacher-table.blade.php
@@ -1,9 +1,9 @@
-<div class="bg-white rounded-2xl shadow-sm border border-gray-100">
+<div class="bg-white rounded-2xl shadow-sm border border-gray-200 dark:border-gray-700 dark:bg-gray-800">
 
     <!-- Header -->
-    <div class="flex items-center justify-between px-6 py-4 border-b border-gray-100">
+    <div class="flex items-center justify-between px-6 py-4 border-b border-gray-200 dark:border-gray-700">
         <div>
-            <h3 class="text-lg font-semibold text-gray-800">
+            <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
                 Enseignants
             </h3>
             <p class="text-sm text-gray-500">
@@ -20,8 +20,8 @@
     <!-- Table -->
     <div class="overflow-x-auto">
         <table class="w-full text-sm text-left">
-            <thead class="bg-gray-50 text-gray-600 uppercase text-xs">
-            <tr>
+            <thead class="bg-gray-50 dark:bg-gray-800 text-gray-600 uppercase text-xs">
+            <tr class="text-left text-xs font-semibold text-gray-500 uppercase tracking-wider dark:text-white">
                 <th class="px-6 py-3">Nom</th>
                 <th class="px-6 py-3">Prénom</th>
                 <th class="px-6 py-3">Email</th>
@@ -29,18 +29,18 @@
             </tr>
             </thead>
 
-            <tbody class="divide-y divide-gray-100">
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
             @forelse($teachers as $teacher)
-                <tr class="hover:bg-gray-50 transition">
-                    <td class="px-6 py-4 font-medium text-gray-800">
+                <tr class="hover:bg-gray-200 dark:hover:bg-gray-700 transition">
+                    <td class="px-6 py-4 font-medium text-gray-800 dark:text-white">
                         {{ $teacher->last_name }}
                     </td>
 
-                    <td class="px-6 py-4 text-gray-600">
+                    <td class="px-6 py-4 text-gray-600 dark:text-white">
                         {{$teacher->first_name}}
                     </td>
 
-                    <td class="px-6 py-4 text-gray-600">
+                    <td class="px-6 py-4 text-gray-600 dark:text-white">
                         {{$teacher->email}}
                     </td>
 

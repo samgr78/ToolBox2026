535a89beab8a40180048fed0e0f5c1f1531d4f84
samuel <samuel@Mac-CF-23-002.local>
2026-06-03 10:59:27 +0200

    TB update : update text in student page

--- resources/views/pages/students/partials/students-table.blade.php
+++ resources/views/pages/students/partials/students-table.blade.php
@@ -2,7 +2,7 @@
 
     <div class="px-6 py-4 border-b border-gray-200 dark:border-gray-700">
         <h3 class="text-lg font-medium text-gray-800 dark:text-white">
-            Mes étudiants
+          Étudiants
         </h3>
     </div>
 
